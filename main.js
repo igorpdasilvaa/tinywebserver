413416399c370e19dc4b20f7c7b7ba92f813a6a0
igorpdasilvaa <igorpdasilvaa@gmail.com>
2024-09-04 21:51:25 -0300

    melhorado suporte a SPA

--- main.js
+++ main.js
@@ -4,13 +4,17 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(morgan('tiny'));
 
 app.use(cors());
 
 app.use(express.static('/website'));
+
+app.use((req, res) => {
+	res.status(404).sendFile('/website/index.html');
+});
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
