86b990f2911f261fcbb39ad30f1740a3745c7620
igorpdasilvaa <igorpdasilvaa@gmail.com>
2024-09-04 22:11:03 -0300

    ajustado porta

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 
